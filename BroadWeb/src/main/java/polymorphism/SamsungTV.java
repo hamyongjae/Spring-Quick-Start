4fe541dd31a1e88cc24861e1bf04338d795a180a
hamyongjae <bbb12388@naver.com>
2018-12-11 08:27:27 +0900

    s s

--- BroadWeb/src/main/java/polymorphism/SamsungTV.java
+++ BroadWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,26 +1,25 @@
 package polymorphism;
 
 public class SamsungTV implements TV{
-	private SonySpeaker speaker;
+	private Speaker speaker;
 	private int price;
 	
 	public SamsungTV() {
-		System.out.println("===> SamsungTV 객체 생성");
+		System.out.println("===> SamsungTV (1) 객체 생성");
 	}
-	
-	public SamsungTV(SonySpeaker speaker) {
-		System.out.println("===> SamsungTV(2) 객체 생성");
+
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 호출");
 		this.speaker = speaker;
 	}
-	
-	public SamsungTV(SonySpeaker speaker, int price) {
-		System.out.println("===> SamsungTV(3) 객체 생성");
-		this.speaker = speaker;
+
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 호출");
 		this.price = price;
 	}
 
 	public void powerOn() {
-		System.out.println("Samsung --- 전원 켠다. (가격 : " + price + ")" );
+		System.out.println("Samsung --- 전원 켠다.  (가격 : " + price + ")" );
 	}
 	
 	public void powerOff() {
@@ -28,10 +27,10 @@ public class SamsungTV implements TV{
 	}
 	
 	public void volumeUp() {
-		System.out.println("SamsungTV --- 소리 올린다.");
+		speaker.volumeUp();
 	}
 	
 	public void volumeDown() {
-		System.out.println("SamsungTV --- 소리 내린다");
+		speaker.volumeDown();
 	}
 }
