01438c3bb28bb7af0cb857501379b2e10e56d550
hamyongjae <bbb12388@naver.com>
2018-12-12 04:38:40 +0900

    12-12 04:38

---
+++ Day01_practice/src/main/java/com/springbook/biz/common/JDBCUtil.java
@@ -0,0 +1,42 @@
+package com.springbook.biz.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCUtil {
+	public static Connection getConnection() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			con = DriverManager.getConnection("jdbc:mariadb://localhost/dbname", "userId", "password");
+//			pstmt = con.prepareStatement("select * from his_bus_voltage");
+//			rs = pstmt.executeQuery();
+//			while (rs.next()) {
+//				// .
+//			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return con;
+	}
+
+	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+
+		try {
+			if (rs != null) rs.close();
+			if (stmt != null) stmt.close();
+			if (conn != null) conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
